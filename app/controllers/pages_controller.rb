52704549067f72674f77fc51874053a21d720f55
Fyodor <fyodor.shvin@gmail.com>
2012-02-03 11:19:09 +0400

    Completed the layout structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
   def home
+  @title = "Dynamic title for Home"
   end
 
   def contact
+  @title = "Dynamic title for Contact"
+  end
+  
+  def about
+  @title = "Dynamic title for About"
+  end
+  
+  def help
+  @title = "Dynamic title for Help"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,10 +8,22 @@ describe PagesController do
       response.should be_success
     end
   end
+  
+  it "should have the right title" do
+  get 'home'
+  response.shoud have_selector("title",:content => "SAMPLE APP"
+  end
 
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
+      response.should be_success
+    end
+  end
+   
+   describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
       response.should be_success
     end
   end
